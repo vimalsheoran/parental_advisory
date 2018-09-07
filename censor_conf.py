8f15a4dead0f138143cb362a884cd2c42e9ac618
Vimal Sheoran <vimalpsheoran@gmail.com>
2018-09-08 00:05:03 +0530

    Adding configs for the provided sample lyrics

--- censor_conf.py
+++ censor_conf.py
@@ -2,8 +2,10 @@ censor_list={
 	"fuck": "f**k",
 	"dick": "d*ck",
 	"weed": "w**d",
+	"bitch": "b*tch",
+	"shit": "sh**",
 }
 
-swear_list = ["fuck", "dick", "weed"]
+swear_list = ["fuck", "dick", "weed", "bitch", "shit"]
 
 LYRICS = "sample_lyrics.txt"
