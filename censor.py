a9b0f04179082540515d44ed6df474ad5cf004cc
Vimal Sheoran <vimalpsheoran@gmail.com>
2018-09-08 00:04:36 +0530

    code cleanup

--- censor.py
+++ censor.py
@@ -3,13 +3,8 @@ from censor_conf import swear_list, censor_list, LYRICS
 
 swear_count = 0
 
-# Reading the lyrics file
-lyrics_file = open(LYRICS, "U")
-
-# for swear in swear_list:
-# 	print swear
-
 # Providing swear word analysis
+lyrics_file = open(LYRICS, "U")
 print("Running Analysis.......")
 time.sleep(1)
 all_words = list(lyrics_file.read().split())
@@ -20,21 +15,24 @@ for swear in swear_list:
 			swear_count += 1
 	print swear+" appears "+str(swear_count)+" times in the song."
 	swear_count = 0
+lyrics_file.close()
 
 # Censoring lyrics
+lyrics_file = open(LYRICS, "U")
 print("Censoring Lyrics...........")
 time.sleep(1)
 raw_lyrics = lyrics_file.read()
 for swear in swear_list:
 	while swear in raw_lyrics:
 		raw_lyrics = raw_lyrics.replace(swear, censor_list[swear])
+lyrics_file.close()
 
 # Outputing censored lyrics
 print("Generating Clean Version.........")
 time.sleep(1)
 with open("censored_lyrics.txt", "w") as f:
 	f.write(raw_lyrics)
-
+	f.close()
 print("Done!")
 
 '''
